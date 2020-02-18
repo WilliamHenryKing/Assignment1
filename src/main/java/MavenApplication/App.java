ad1cbc49f72992a3e5c59446ea0c26a60791ec61
Lance <lancejohnston3@gmail.com>
2020-02-18 19:04:57 +0200

    first commit

---
+++ src/main/java/MavenApplication/App.java
@@ -0,0 +1,15 @@
+package MavenApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "hello world" );
+        System.out.println( "this my first commit" );
+    }
+}
